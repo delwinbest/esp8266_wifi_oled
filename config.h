231df1336de637fc5096e4ac339c0b99cf02fca4
Delwin Best <delwin@laava.id>
2018-12-30 21:08:32 -0500

    Cleanup

--- config.h
+++ config.h
@@ -8,14 +8,12 @@ const char* HOSTNAME = "ESP_OLED";
 const char* ssid     = "SSID";
 const char* password = "password";
 
+// ESP Wifi oled Board Pin Defnitions
+const byte  pin_UP = 12;
+const byte  pin_DOWN = 14;
+const byte  pin_ENTER = 13;
 // D5 (Arduino), 1 NodeMCU -> SDA
 // D4 (Arduino), 2 NodeMCU -> SCL
 const byte  pin_SDA = 5;
 const byte  pin_SCL = 4;
-
 const byte  pin_LED = 16;
-
-// Probably wrong
-const byte  pin_UP = 12;
-const byte  pin_DOWN = 14;
-const byte  pin_ENTER = 13;
